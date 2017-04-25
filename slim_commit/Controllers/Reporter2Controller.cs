57f4dc08ff7fbc81b88e10fe03904ad075ed08bc
Karan Bir <karan@upeo.com>
2017-04-25 22:18:30 +0530

    Reporter2 api implementation

---
+++ slim_commit/Controllers/Reporter2Controller.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.Http;
+using slim_commit.Models;
+
+namespace slim_commit.Controllers
+{
+    /// <summary>
+    /// Reporter Controller
+    /// </summary>
+    public class Reporter2Controller : BaseApiController
+    {
+        /// <summary>
+        /// Connection string
+        /// </summary>
+        private readonly string _connectionString = ConfigurationManager.ConnectionStrings["tapr"].ConnectionString;
+
+         
+        /// <summary>
+        /// Get Campuses
+        /// </summary> 
+        /// <returns></returns>
+        [HttpPost]
+        public List<Reporter2TaprModel> GetTaprData(Reporter2TaprRequestModel filter)
+        {
+            var records = new List<Reporter2TaprModel>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("select * from [dbo].[cstud] where [year] = '2016' and Attr IN (@attrs)", connection);
+                command.AddArrayParameters(filter.TaprList, "attrs");
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    try
+                    {
+                        records.Add(new Reporter2TaprModel(reader));
+                    }
+                    catch { }
+                }
+                connection.Close();
+            }
+            return records;
+        }
+
+        /// <summary>
+        /// Get GradeTypes
+        /// </summary>
+        /// <returns></returns>
+        public List<string> GetGradeTypes()
+        {
+            var records = new List<string>();
+
+            using (var connection = new SqlConnection(_connectionString))
+            {
+                connection.Open();
+                var command = new SqlCommand("select  DISTINCT GRDTYPE from reporterC2C", connection);
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    try
+                    {
+                        records.Add(reader["GRDTYPE"].ToString());
+                    }
+                    catch { }
+                }
+                connection.Close();
+            }
+            return records;
+        }
+    }
+}
+     
---
+++ slim_commit/Models/Reporter2Model.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace slim_commit.Models
+{
+    public class Reporter2TaprRequestModel
+    {
+        public string[] TaprList { get; set; }
+    }
+
+    public class Reporter2TaprModel
+    {
+        public int Id { get; set; }
+        public string Campus { get; set; }
+        public string Attr { get; set; }
+        public string Year { get; set; }
+        public string Val { get; set; }
+
+        /// <summary>
+        /// Constructor implementation
+        /// </summary>
+        /// <param name="dataRecord"></param>
+        public Reporter2TaprModel(IDataRecord dataRecord)
+        {
+            this.Id = Convert.ToInt32(dataRecord["Id"]);
+            this.Campus = dataRecord["Campus"].ToString();
+            this.Attr = dataRecord["Attr"].ToString();
+            this.Year = dataRecord["Year"].ToString();
+            this.Val = dataRecord["Val"].ToString();
+        }
+    }
+}
